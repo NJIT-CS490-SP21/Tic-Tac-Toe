aab3882b8eaa9fc893339f17f73f1b339165bbd4
Hang Nguyen <kathlyn.hang.nguyen11@gmail.com>
2021-03-15 04:11:24 +0000

    pylint models.py

--- models.py
+++ models.py
@@ -1,17 +1,13 @@
+"""
+Module contains Player model for database
+"""
 from app import DB
 
-
 class Player(DB.Model):
-    __table_args__ = {'extend_existing': True}
-    id = DB.Column(DB.Integer, primary_key=True)
-    username = DB.Column(DB.String(80), unique=True, nullable=False)
-    score = DB.Column(DB.Integer, nullable=False)
-
-    def __repr__(self):
-        return '<Test %r>' % self.username
-
-
-class Test(DB.Model):
+    """
+    Player class to contain information about player
+    """
+    # pylint: disable=too-few-public-methods
     __table_args__ = {'extend_existing': True}
     id = DB.Column(DB.Integer, primary_key=True)
     username = DB.Column(DB.String(80), unique=True, nullable=False)
